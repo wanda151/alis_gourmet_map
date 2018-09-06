4c6bacdf71dc6956d0f5ddee1a9a8b66bbe1ca2b
wanda151 <carol720wander@gmail.com>
2018-09-06 15:00:06 +0900

    Fix gspread and (for in s) etc.

--- alisgourmetmap_test.py
+++ alisgourmetmap_test.py
@@ -10,61 +10,44 @@ import requests
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+
 api_tag = 'https://alis.to/api/search/articles?tag=%E8%87%B3%E9%AB%98%E3%81%AE%E2%97%8B%E2%97%8B%E3%83%86%E3%82%B9%E3%83%88'
-#APIの/search/articlesのパラメーターに「至高の○○テスト」と入力後、queryをtagに変換
-
 url_tag = urllib.request.urlopen(api_tag)
-article_tag = json.loads(url_tag.read().decode("utf-8"))
-article_id = [article_tags.get('article_id') for article_tags in article_tag]
-print(article_id)
-#article_idのみ取り出す
+article_tags = json.loads(url_tag.read().decode("utf-8"))
+article_ids = [article_tag.get('article_id') for article_tag in article_tags]
+#article idを取り出す
 
-user_id = [article_tags.get('user_id') for article_tags in article_tag]
-print(user_id)
-#user_idのみ取り出す
+user_ids = [article_tag.get('user_id') for article_tag in article_tags]
+#user id を取り出す
 
-api_article_id = ["https://alis.to/api/articles/"+article_id for article_id in article_id]
-print(api_article_id)
-#article_idのapiを使いやすい形にする
+api_article_ids = ["https://alis.to/api/articles/"+article_id for article_id in article_ids]
+#article_idをAPIで取り出しやすい形にする
 
-user_id_info_api = ["https://alis.to/api/users/"+user_ids+"/info" for user_ids in user_id]
-#user_idのapiを使いやすい形にする
+user_id_info_apis = ["https://alis.to/api/users/"+user_id+"/info" for user_id in user_ids]
+#user_id_infoをAPIで取り出しやすい形にする
 
-user_id_info = [json.loads(urllib.request.urlopen(user_id_info_apis).read().decode("utf-8")) for user_id_info_apis in user_id_info_api]
-user_display_name = [user_id_infos.get('user_display_name') for user_id_infos in user_id_info]
-for user_display_names in user_display_name:
-    print(user_display_names)
-    #user_display_nameを取り出す
+user_id_infos = [json.loads(urllib.request.urlopen(user_id_info_api).read().decode("utf-8")) for user_id_info_api in user_id_info_apis]
+#/users/{user_id}/infoを取り出す
 
-likes_api = ["https://alis.to/api/articles/"+article_ids+"/likes" for article_ids in article_id]
-#likesのapiを使いやすい形にする
+user_display_names = [user_id_info.get('user_display_name') for user_id_info in user_id_infos]
+#user_display_name　を取り出す
 
-like = [json.loads(urllib.request.urlopen(likes_apis).read().decode("utf-8")) for likes_apis in likes_api]
-pprint.pprint(like)
-likes_count = [likes.get('count') for likes in like]
-for likes_counts in likes_count:
-    print(likes_counts)
-    #like数を取り出す
+likes_apis = ["https://alis.to/api/articles/"+article_id+"/likes" for article_id in article_ids]
+#likes_apiをAPIで取り出しやすい形にする
+likes = [json.loads(urllib.request.urlopen(likes_api).read().decode("utf-8")) for likes_api in likes_apis]
+likes_counts = [like.get('count') for like in likes]
+#Like数　を取り出す
 
-articles_body = [json.loads(urllib.request.urlopen(api_article_ids).read().decode("utf-8")) for api_article_ids in api_article_id]
-pprint.pprint(articles_body)
-#記事の本文を取りだす
-
+articles_bodys = [json.loads(urllib.request.urlopen(api_article_id).read().decode("utf-8")) for api_article_id in api_article_ids]
 def get_block(text, start_text, end_text):
+    if not text.find(start_text) >= 0:
+        return []
     new_texts = []
     for split_text in text.split(start_text):
-        if split_text.find(end_text):
+        if split_text.find(end_text) >= 0:
             new_texts.append(split_text.split(end_text)[0])
     return new_texts
-for articles_bodys in articles_body:
-    texts = get_block(articles_bodys.get("body"), "<blockquote>", "</blockquote>")
-    print(texts[1])
-    #<blockquote>内の文を取り出す
 
-for (article_ids, user_ids) in zip(article_id, user_id):
-    article_url="https://alis.to/"+str(user_ids) +"/articles/"+ str(article_ids)
-    print(article_url)
-    #記事URLを取り出す
 
 
 
@@ -80,28 +63,62 @@ workbook = gc.open_by_key('1u3J0zY9CXc4zHPtcrWDeGNkazFaCPVmObpu3CWVVvXA')
 worksheet = workbook.sheet1
 #sheet1を選択する
 
-u = 1
-for articles_bodys in articles_body:
-    texts = get_block(articles_bodys.get("body"), "<blockquote>", "</blockquote>")
-    u = u+1
-    worksheet.update_cell(u, 1, texts[1])
-    #スプレッドシートのA列に<blockquote>内の文を書き込む
+for i, articles_body in enumerate(articles_bodys):
+    texts = get_block(articles_body.get("body"), "<blockquote>", "</blockquote>")
+    texts = [text for text in texts if text.find("〒") >= 0]
+    for text in texts:
+        texts = get_block(articles_body.get("body"), "企画名", "<br>料理ジャンル")
+    project_name_str = str(texts)
+    print(project_name_str.replace(":", "").replace("：", "").replace("['","").replace("']",""))
+    project_name = project_name_str.replace(":", "").replace("：", "").replace("['","").replace("']","")
+    worksheet.update_cell(i+2, 1, project_name)
+    #スプレッドシートのA列に企画名を書き込む
 
-v = 1
-for (article_ids, user_ids) in zip(article_id, user_id):
-    article_url="https://alis.to/"+str(user_ids) +"/articles/"+ str(article_ids)
-    v = v+1
-    worksheet.update_cell(v, 3, article_url)
-    #スプレッドシートのC列に記事URLを書き込む
+for i, articles_body in enumerate(articles_bodys):
+    texts = get_block(articles_body.get("body"), "<blockquote>", "</blockquote>")
+    texts = [text for text in texts if text.find("〒") >= 0]
+    for text in texts:
+        texts = get_block(articles_body.get("body"), "料理ジャンル", "<br>店名")
+    food_Genre_str = str(texts)
+    print(food_Genre_str.replace(":", "").replace("：", "").replace("['","").replace("']",""))
+    food_Genre = food_Genre_str.replace(":", "").replace("：", "").replace("['","").replace("']","")
+    worksheet.update_cell(i+2, 2, food_Genre)
+    #スプレッドシートのB列に記事URLを書き込む
+    
+for i, articles_body in enumerate(articles_bodys):
+    texts = get_block(articles_body.get("body"), "<blockquote>", "</blockquote>")
+    texts = [text for text in texts if text.find("〒") >= 0]
+    for text in texts:
+        texts = get_block(articles_body.get("body"), "店名：", "<br>住所")
+    store_name_str = str(texts)
+    print(store_name_str.replace(":", "").replace("：", "").replace("['","").replace("']",""))
+    store_name = store_name_str.replace(":", "").replace("：", "").replace("['","").replace("']","")
+    worksheet.update_cell(i+2, 3, store_name)
+    #スプレッドシートのC列に店名を書き込む
 
-w = 1
-for user_display_names in user_display_name:
-    w=w+1
-    worksheet.update_cell(w, 4, user_display_names)
-    #スプレッドシートのD列にユーザー名を書き込む
+for i, articles_body in enumerate(articles_bodys):
+    texts = get_block(articles_body.get("body"), "<blockquote>", "</blockquote>")
+    texts = [text for text in texts if text.find("〒") >= 0]
+    for text in texts:
+        texts = get_block(articles_body.get("body"), "<br>住所：", "</blockquote>")
+        texts = [text for text in texts if text.find("〒") >= 0]
+    location_str = str(texts)
+    print(location_str.replace(":", "").replace("：", "").replace("['","").replace("']",""))
+    location = location_str.replace(":", "").replace("：", "").replace("['","").replace("']","")
+    worksheet.update_cell(i+2, 4, location)
+    #スプレッドシートのD列に住所を書き込む
 
-y = 1
-for likes_counts in likes_count:
-    y=y+1
-    worksheet.update_cell(y, 5, likes_counts)
-    #スプレッドシートのE列にLike数を書き込む
+
+for i, (article_id, user_id) in enumerate(zip(article_ids, user_ids)):
+    article_url="https://alis.to/"+str(user_id) +"/articles/"+ str(article_id)
+    worksheet.update_cell(i+2, 5, article_url)
+    #スプレッドシートのE列に記事URLを書き込む
+
+for i, user_display_name in enumerate(user_display_names):
+    worksheet.update_cell(i+2, 6, user_display_name)
+    #スプレッドシートのF列にユーザー名を書き込む
+
+
+for i, likes_count in enumerate(likes_counts):
+    worksheet.update_cell(i+2, 7, likes_count)
+    #スプレッドシートのG列にLike数を書き込む
